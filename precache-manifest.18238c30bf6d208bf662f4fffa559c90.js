8c5d8963c4f5b04d653af9855d6137ece1727a88
David Zetterdahl <david.zetterdahl@hiq.se>
2020-01-22 16:56:54 +0100

    Updates

---
+++ precache-manifest.18238c30bf6d208bf662f4fffa559c90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019183a9b0bd7739440ad647bce910d6",
+    "url": "/automatblog/index.html"
+  },
+  {
+    "revision": "ef9d5065b45760a6b6b0",
+    "url": "/automatblog/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b480f206dad77a09ed2c",
+    "url": "/automatblog/static/js/2.ee7ba151.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/automatblog/static/js/2.ee7ba151.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef9d5065b45760a6b6b0",
+    "url": "/automatblog/static/js/main.f60ab9d4.chunk.js"
+  },
+  {
+    "revision": "861f2e4493e257930d77",
+    "url": "/automatblog/static/js/runtime-main.1c1af28e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/automatblog/static/media/logo.5d5d9eef.svg"
+  }
+]);
